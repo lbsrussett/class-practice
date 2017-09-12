f1497b77d767ab931ec0866e3b075a53350fb85d
lbsrussett <lbsrussett@gmail.com>
2017-09-12 12:38:37 -0600

    created conflict with branch

--- src/sample.java
+++ src/sample.java
@@ -3,11 +3,11 @@ public static void main(String [] args) {
 
     System.out.println("I am testing a branching method.");
 
-<<<<<<< HEAD
-=======
     System.out.println("This is another test.");
 
     System.out.println("Adding more code.");
     
->>>>>>> mybranch
+    pubilc void dummy() {
+    	System.out.println("Conflict method");
+    }
 }

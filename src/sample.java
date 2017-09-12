ba182d524bc721d1e68860188871a9ab66a39724
lbsrussett <lbsrussett@gmail.com>
2017-09-12 12:28:41 -0600

    changed sample.java on master branch.

--- src/sample.java
+++ src/sample.java
@@ -2,4 +2,6 @@ public static void main(String [] args) {
     System.out.println("This is a sample program.");
 
     System.out.println("I am testing a branching method.");
+
+    System.out.println("This is a commit from the master branch.");
 }
